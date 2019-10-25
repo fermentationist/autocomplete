537d07ca10d8466d0df33963a3946e7041e27045
Dennis Hodges <fermentationist@gmail.com>
2019-10-25 12:34:45 -0500

    Autocomplete functional

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Autocomplete from "./Autocomplete.js";
+// import logo from './logo.svg';
+// import './App.css';
 
 function App() {
+  const suggestions = [
+    "Rick Astley",
+    "The Clash",
+    "The Cure",
+    "Barry White",
+    "Barry Manilow",
+    "Fleetwood Mac",
+    "Harry Nilsson",
+    "Heart",
+    "Barbara Streisand",
+    "White Lion",
+    "White Stripes",
+
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Autocomplete suggestions={suggestions} />
     </div>
   );
 }
